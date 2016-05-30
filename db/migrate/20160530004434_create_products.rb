7bed61342219a1da8daf51d539ec33b49223f977
Anna Sobolewska <sobolewska.anna@gmail.com>
2016-05-29 17:49:50 -0700

    products migration

--- db/migrate/20160530004434_create_products.rb
+++ db/migrate/20160530004434_create_products.rb
@@ -1,6 +1,11 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
+      t.string :description
+      t.string :image_url
+      t.string :store_url
+      t.integer :user_id
+      t.integer :show_id
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160529223447) do
+ActiveRecord::Schema.define(version: 20160530004434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string   "description"
+    t.string   "image_url"
+    t.string   "store_url"
+    t.integer  "user_id"
+    t.integer  "show_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "shows", force: :cascade do |t|
     t.string   "title"
