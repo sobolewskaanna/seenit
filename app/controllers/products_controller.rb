1e027ae7c79fed0ab3d99ac015505b5e5690761c
Anna Sobolewska <sobolewska.anna@gmail.com>
2016-05-29 21:28:54 -0700

    products new and create

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,25 @@
 class ProductsController < ApplicationController
+
   def index
     @show = Show.where(id: params[:show_id])
-    p '********************'
-    p @show
+    @products = Product.where(show_id: params[:show_id])
   end
+
+  def new
+    @product = Product.new
+    render :new
+  end
+
+  def create
+    @product = Product.create(product_params)
+    @product.show_id = params[:show_id]
+    if @product.save
+      redirect_to show_products_path
+    end
+  end
+
+  def product_params
+    params.require(:product).permit(:description, :image_url, :store_url)
+  end
+
 end
