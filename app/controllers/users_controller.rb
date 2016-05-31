c0806d856eecf2f94195e3d0da1e1a0bf94eb287
Anna Sobolewska <sobolewska.anna@gmail.com>
2016-05-30 22:34:55 -0700

    following shows added to user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ before_action :authenticate_user!
 
   def show
     @user = User.find(params[:id])
+    @follows = Follow.where(user_id: current_user)
     if current_user
       render :show
     end
