5712b4123196d2bd46a3ccf1ae4b5866ad8a10aa
Anna Sobolewska <sobolewska.anna@gmail.com>
2016-05-30 19:48:52 -0700

    users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
 before_action :authenticate_user!
 
+  def show
+    @user = User.find(params[:id])
+    @follows = Follow.where(user_id: current_user.id)
+    if current_user
+      render :show
+    end
+  end
+
 end
