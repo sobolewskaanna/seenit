9127621933feb57469130bb8ecf3830a53ede73a
Anna Sobolewska <sobolewska.anna@gmail.com>
2016-05-30 20:19:25 -0700

    create and destroy routes for follow

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get '/products', to: "products#index", as: 'products'
   resources :users, only: [:show]
   resources :shows, only: [:index], shallow: true do
+    resources :follows, only: [:create]
+    delete "/follows/:id", to: "follows#destroy", as: "delete_follow"
     resources :products
   end
 
